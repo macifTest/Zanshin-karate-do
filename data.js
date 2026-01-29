8926680049a38796606451414ace41fcc228b844
Emmanuel S <32673960+macifTest@users.noreply.github.com>
2026-01-29 23:22:06 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+
+
+const DATA = {
+    positions: [
+        "Zenkutsu-Dachi", 
+        "Kokutsu-Dachi", 
+        "Kiba-Dachi", 
+        "Fudo-Dachi", 
+        "Neko-Ashi-Dachi"
+    ],
+    directions: [
+        "En avançant", 
+        "En reculant", 
+        "Tai Sabaki (Rotation)", 
+        "Sur place"
+    ],
+    techniques: [
+        "Uraken Uchi Jodan", 
+        "Haito Uchi Chudan", 
+        "Mae Empi Uchi", 
+        "Yoko Geri Kekomi", 
+        "Ushiro Geri", 
+        "Shuto Uke", 
+        "Gyaku Tsuki Chudan", 
+        "Kizami Tsuki Jodan", 
+        "Teisho Uchi",
+        "Age Uke", 
+        "Mawashi Geri", 
+        "Soto Uke", 
+        "Ura Mawashi Geri"
+    ]
+};
